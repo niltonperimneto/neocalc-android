dc158a36cae9292f163d1e455fd04e033f64eedd
Nilton Perim Neto <niltonperimneto@gmail.com>
2026-07-05 02:50:42 -0300

    build: upgrade to JDK 21 and update CI Actions dependencies
    - Upgrade Java source/target compatibility and Kotlin JVM target to version 21 in build.gradle.kts.
    - Configure GitHub Actions CI workflow to use JDK 21.
    - Bump actions/checkout version to v7.
    - Bump softprops/action-gh-release version to v3.
    
    Signed-off-by: Nilton Perim Neto <niltonperimneto@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -142,6 +142,6 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
